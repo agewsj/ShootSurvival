3af250d8c5ebd4fc3eba6b64af14c0f72e489fc5
agewsj@naver.com <agewsj@naver.com>
2019-08-07 22:45:55 +0900

    돌튕기기 게임

---
+++ Assets/Stone/Scripts/MapLoadManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapLoadManager : MonoBehaviour
+{
+    public GameObject mapPrefab = null;
+    public Transform stone = null;    
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+}
---
+++ Assets/Stone/Scripts/Stone.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stone : MonoBehaviour
+{
+    Rigidbody rigidbody = null;
+    public float moveSpeed = 0;
+    public int jumpPower = 0;
+    public bool isJump = false;
+
+    void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Jump();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            if(isJump == false)
+            {
+                isJump = true;
+                Jump();
+            }            
+        }
+    }
+
+    void Move()
+    {
+        transform.position += new Vector3(moveSpeed, 0f, 0f);
+    }
+
+    void Jump()
+    {
+        rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.gameObject.CompareTag("Jump"))
+        {         
+            isJump = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Jump"))
+        {            
+            isJump = true;
+        }
+    }
+}
